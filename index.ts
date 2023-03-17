04d53f52998672a55f9894fb0690463f81ae166d
SMooreSwe <118370783+SMooreSwe@users.noreply.github.com>
2023-03-17 08:55:23 +0100

    DB routing much more efficient

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ const port = 8080;
 const cors=require("cors");
 const corsOptions ={
    origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
+   credentials:true,          
    optionSuccessStatus:200,
 }
 
--- src/db.ts
+++ src/db.ts
@@ -3,6 +3,23 @@ import { ObjectId } from 'mongodb';
 
 require('dotenv').config();
 
+let db : mongoDB.Db;
+let col: mongoDB.Collection;
+
+const init = async () => {
+    const client: mongoDB.MongoClient = new mongoDB.MongoClient(
+        `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@hackdayidea.bdc1jdl.mongodb.net/?retryWrites=true&w=majority`,
+      );
+    try {
+        await client.connect();
+        db = await client.db(`${process.env.MONGO_DB}`);
+        col = db.collection(`${process.env.MONGO_COLLECTION}`);
+        console.log('Connected successfully to server');
+    } catch (err) {
+        console.error(err);
+    }
+};
+
 type User = {
     _id?: ObjectId,
     username: string,
@@ -11,56 +28,30 @@ type User = {
 }
 
 export const getOne = async (username: string, password: string) => {
-    const client: mongoDB.MongoClient = new mongoDB.MongoClient(
-      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@hackdayidea.bdc1jdl.mongodb.net/?retryWrites=true&w=majority`,
-    );
-    await client.connect();
-    const db: mongoDB.Db = client.db(`${process.env.MONGO_DB}`);
-    const col: mongoDB.Collection = db.collection(`${process.env.MONGO_COLLECTION}`);
     const data = (await col.find<User>({ username: `${username}`, password: `${password}` }).toArray());
-    await client.close();
     return data;
 };
 
 export const saveQuote = async (id: ObjectId, quote: string) => {
-    const client: mongoDB.MongoClient = new mongoDB.MongoClient(
-      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@hackdayidea.bdc1jdl.mongodb.net/?retryWrites=true&w=majority`,
-    );
-    await client.connect();
-    const db: mongoDB.Db = client.db(`${process.env.MONGO_DB}`);
-    const col: mongoDB.Collection = db.collection(`${process.env.MONGO_COLLECTION}`);
     const data = (await col.updateOne({ _id: new ObjectId(id) },
      { $push: {savedQuotes: `${quote}`}}));
-    await client.close();
     return data;
 };  
 
 export const deleteQuote = async (id: ObjectId, quote: string) => {
-    const client: mongoDB.MongoClient = new mongoDB.MongoClient(
-        `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@hackdayidea.bdc1jdl.mongodb.net/?retryWrites=true&w=majority`,
-    );
-    await client.connect();
-    const db: mongoDB.Db = client.db(`${process.env.MONGO_DB}`);
-    const col: mongoDB.Collection = db.collection(`${process.env.MONGO_COLLECTION}`);
     const data = (await col.updateOne({ _id: new ObjectId(id) },
      { $pull: {savedQuotes: `${quote}`}}));
-    await client.close();
     return data;
 }
 
 export const createUser = async (username: string, password: string) => {
-    const client: mongoDB.MongoClient = new mongoDB.MongoClient(
-        `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@hackdayidea.bdc1jdl.mongodb.net/?retryWrites=true&w=majority`,
-    );
-    await client.connect();
-    const db: mongoDB.Db = client.db(`${process.env.MONGO_DB}`);
-    const col: mongoDB.Collection = db.collection(`${process.env.MONGO_COLLECTION}`);
     const newUser = {
         username: username,
         password: password,
         savedQuotes: [],
     }
     const data = await col.insertOne(newUser);
-    await client.close();
     return data;
 }
+
+init();
